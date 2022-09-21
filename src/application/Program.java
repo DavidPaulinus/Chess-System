fa1f3af0312d16a701830a76c36d9b29562c38cb
Davi Paulino <davipm@hotmail.com>
2022-09-21 10:01:45 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Position posi=new Position(3,5);
+		
+		System.out.println(posi);
+		
 	}
 
 }
